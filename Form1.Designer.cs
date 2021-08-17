85dff91163e9553730c281ba54068d2e97af4434
ana Soares Silva <anaclara.soaress@icloud.com>
2021-08-17 17:35:08 -0300

    Aula de operadores aritmeticos

---
+++ Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿
+namespace Aula1_TAP
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtResultado = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnResultado = new System.Windows.Forms.Button();
+            this.txtMin = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtMax = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtResultado);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.btnResultado);
+            this.groupBox1.Controls.Add(this.txtMin);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.txtMax);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.groupBox1.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(627, 263);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Cálculo da área do Losango";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // txtResultado
+            // 
+            this.txtResultado.Location = new System.Drawing.Point(142, 209);
+            this.txtResultado.Name = "txtResultado";
+            this.txtResultado.Size = new System.Drawing.Size(100, 34);
+            this.txtResultado.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(18, 212);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(118, 28);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "RESULTADO";
+            // 
+            // btnResultado
+            // 
+            this.btnResultado.Location = new System.Drawing.Point(168, 151);
+            this.btnResultado.Name = "btnResultado";
+            this.btnResultado.Size = new System.Drawing.Size(106, 43);
+            this.btnResultado.TabIndex = 4;
+            this.btnResultado.Text = "button1";
+            this.btnResultado.UseVisualStyleBackColor = true;
+            this.btnResultado.Click += new System.EventHandler(this.btnResultado_Click);
+            // 
+            // txtMin
+            // 
+            this.txtMin.Location = new System.Drawing.Point(355, 117);
+            this.txtMin.Name = "txtMin";
+            this.txtMin.Size = new System.Drawing.Size(100, 34);
+            this.txtMin.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 120);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(343, 28);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Informe a menor diagonal do losango";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // txtMax
+            // 
+            this.txtMax.Location = new System.Drawing.Point(355, 75);
+            this.txtMax.Name = "txtMax";
+            this.txtMax.Size = new System.Drawing.Size(100, 34);
+            this.txtMax.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 78);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(337, 28);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Informe a maior diagonal do losango";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtMax;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtMin;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtResultado;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnResultado;
+    }
+}
+
